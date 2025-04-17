3f437335128959702d4b478e1a51901501d2c870
Snehagowda02 <121510738+Snehagowda02@users.noreply.github.com>
2025-04-17 06:14:45 +0000

    basics

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,38 @@
+// const ID = 5352353
+// const names = "sjiufhgyuguyno"
+// let address = "Chikkmagalur"
+// var DOB = "kjhfnsf" 
+// // not to use var
+// // because of block scope
+
+// console.log(ID);
+// console.table([ID, names, address, DOB])
+// // names = "fjkhfg"
+// address = "dfhufui"
+// DOB= 5325325
+// console.table([ID, names, address, DOB])
+
+// //  null is a standalone value
+// let state = null
+// //  undefined value not assigned
+// console.log(typeof"Snkjgggfjhs")
+// //  type of null is a object 
+// //  type of undefined is undefined 
+// console.log(typeof state)
+
+let value = undefined
+let valueINnumber = Number(value)
+console.log(value)
+console.table([value, typeof value])
+console.table([valueINnumber, typeof valueINnumber])
+
+//  value "33" => converts it into number
+//  value "33abs" => NaN - Not a Number
+//  value true => 1, false=> 0
+
+//  convering ther boolean
+let boolvalue= ""
+
+value = Boolean(boolvalue)
+console.log(value)
+//  when empty st5ring gets converted into , givves false, number 0
